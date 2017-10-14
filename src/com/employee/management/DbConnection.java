626da48ad480952845738c70cd2d8060c0965016
Mert Akel <akelmert95@gmail.com>
2017-10-14 20:02:58 +0300

    first commit

---
+++ src/com/employee/management/DbConnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.employee.management;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author makel
+ */
+ /*   String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://localhost/EMP";
+*/
+
+public class DbConnection {
+    
+    Connection conn = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+     
+    //  Database credentials
+       static final String USER = "root";
+       static final String PASS = "241m1e2r3t4";
+
+    
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/ems",USER,PASS);
+            return conn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+    
+}
